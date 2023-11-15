fab5ec699b08125a56f1d63f31222705295a193e
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-11-15 13:25:15 +0100

    Post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import '../styles/Post.css';
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteOutline } from "react-icons/md";
 
 interface PostProps {
     userId: number;
@@ -8,10 +11,20 @@ interface PostProps {
 
 export function Post({ userId, title, body }: PostProps) {
     return (
-        <div>
-            <p>{userId}</p>
-            <h3>{title}</h3>
-            <p>{body}</p>
+        <div className='post'>
+            <div className='info'>
+                <p>Creator: {userId}</p>
+                <h3>{title}</h3>
+                <p>{body}</p>
+            </div>
+            <div className='btns'>
+                <button className='btn edi'>
+                    <CiEdit className='edit' />
+                </button>
+                <button className='btn del'>
+                    <MdDeleteOutline className='delete' />
+                </button>
+            </div>
         </div>
     );
 };
