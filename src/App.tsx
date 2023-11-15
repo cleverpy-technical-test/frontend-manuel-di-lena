deadf712989a15e645b5955da1a20e188ebb589c
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-11-15 12:41:26 +0100

    Menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import { PostList } from './components/PostList';
+import { Menu } from './components/Menu';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
     return (
-        <div>
-            <div>
+        <div className='main'>
+            <Menu />
+            <div className='container'>
+                <Sidebar />
                 <PostList />
             </div>
         </div>
---
+++ src/components/Menu.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/Menu.css';
+
+export function Menu() {
+    return (
+        <div className='menu'>
+                <h1 className='title'>Technical Test</h1>
+                <button className='btnLogin'>Login</button>
+        </div>
+    );
+};
